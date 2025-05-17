cb8b38b6a744c4d203a8b5783ffb6db68b1bfa9f
Michael Ireland <michael.ireland@meetdandy.com>
2025-05-16 20:30:13 -0500

    adding more dependencies

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { QueryFailedError } from 'typeorm';
 
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log("QueryFailedError", QueryFailedError);
     return 'Hello World!';
   }
 }
